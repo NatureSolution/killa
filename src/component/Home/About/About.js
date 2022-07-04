6174e1dd60b1ba8a30b19fd9c0fc1e5050ac791c
syadur-wdd <syadur.wdd@gmail.com>
2022-07-04 19:49:18 +0600

    Test file

--- src/component/Home/About/About.js
+++ src/component/Home/About/About.js
@@ -68,6 +68,24 @@ const About = () => {
           eaque!
         </p>
       </div>
+
+      <div>
+        <h2>Test Killa</h2>
+        <p className="w-75 p-3 mx-auto">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+          deserunt velit distinctio quas cupiditate adipisci tenetur, explicabo
+          voluptate nostrum aliquid, praesentium a, mollitia libero nesciunt
+          tempore eos excepturi aperiam. Accusantium laudantium nostrum, nemo
+          omnis numquam, eius quam adipisci, dolor alias impedit sapiente veniam
+          veritatis esse amet in consectetur ipsam eum mollitia sint sequi
+          necessitatibus at dolores. Vero quibusdam consequatur optio mollitia
+          voluptate cupiditate adipisci provident doloremque debitis voluptatem,
+          eos impedit veritatis nam corporis dolorum laborum rerum earum. Atque
+          vitae eum sint nemo, quisquam ea et molestiae? Aut, obcaecati iusto.
+          Natus aliquam sint repellendus? Laborum nostrum amet nisi sunt sit
+          eaque!
+        </p>
+      </div>
     </div>
   );
 };
